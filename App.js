6f21daa85f5ba05c9dde669c8bdb6944812d3ba3
Jun Lee <jun@Juns-MacBook-Pro-2.local>
2021-02-22 22:28:25 -0800

    graphql implementation

--- App.js
+++ App.js
@@ -1,23 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { ApolloProvider } from '@apollo/client';
+import client from './graphql/client'
+import Main from './screen/Main'
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ApolloProvider client={client}>
+      <View>
+        <Main />
+        <StatusBar style="auto" />
+      </View>
+    </ApolloProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
 export default App
