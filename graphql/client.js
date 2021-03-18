3ef84224f11c1d56082ecb57d77a7fa310be1108
Jun Lee <jun@Juns-MacBook-Pro-2.local>
2021-03-17 18:53:50 -0700

    optimize ios

--- graphql/client.js
+++ graphql/client.js
@@ -1,24 +1,34 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client'
 import { setContext } from '@apollo/client/link/context'
+import * as Device from 'expo-device'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 const httpLink = createHttpLink({
   uri: 'http://localhost:3000/graphql',
   credentials: 'same-origin'
 })
 
-const authLink = setContext((_, { headers }) => {
-  const token = document.cookie
-  .split('; ')
-  .find(row => row.startsWith('token='))
-  .split('=')[1]
+const getToken = async () => {
+  const device = await Device.getDeviceTypeAsync()
+  let token
+  if (device === Device.DeviceType.DESKTOP) {
+    token = document.cookie.split('; ').find(row => row.startsWith('token='))
+  } else {
+    token = await AsyncStorage.getItem('@token')
+  }
+  token = token.split('=')[1]
+  return token ? `Bearer ${token}` : ''
+}
 
+const authLink = setContext( async (_, { headers }) => {
+  const token = await getToken()
   return {
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}` : '',
+      authorization: token
     }
   }
-});
+})
 
 const client = new ApolloClient({
   link: authLink.concat(httpLink),
--- src/context/userContext.js
+++ src/context/userContext.js
@@ -1,6 +1,8 @@
 import React, { createContext, useState, useEffect } from 'react'
 import { useQuery } from '@apollo/client'
 import CurrentUser from '../../graphql/query/currentUser.gql'
+import * as Device from 'expo-device'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 export const UserContext = createContext()
 
@@ -15,8 +17,18 @@ const UserProvider = ({children}) => {
   },[loading, data])
 
   useEffect(()=> {
-    document.cookie = `token=${currentUser?.token || ''}`
+    setCookie()
   },[currentUser])
+
+  const setCookie = async () => {
+    const device = await Device.getDeviceTypeAsync()
+    const value = `token=${currentUser?.token || ''}`
+    if (device === Device.DeviceType.DESKTOP) {
+      document.cookie = value
+    } else {
+      await AsyncStorage.setItem('@token', value)
+    }
+  }
 
   return (
     <UserContext.Provider
